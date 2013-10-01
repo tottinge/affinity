afc30fd9810e172e7fe4fe08ed59b57d6d888dae
Tim Ottinger <ottingertim@deere.com>
2013-10-01 09:26:22 -0500

    using nx.edge_boundary we get better information quality/quantity

--- display_connectors.py
+++ display_connectors.py
@@ -9,19 +9,13 @@ def find_bridge_edges(raw, squelched):
     groups = build_affinity_groups(squelched)
     for (left,right) in itertools.combinations(groups, 2):
 
-        left_node = left.nodes()[0]
         left_name = name_subgraph(left)
-        
-        right_node = right.nodes()[0]
         right_name = name_subgraph(right)
 
-        try:
-            bridge = set(nx.shortest_path(raw, left_node, right_node))
-            chain = bridge .difference(left.nodes()) .difference(right.nodes())
-            if chain:
-                yield left_name, right_name, list(name_for(int(x)) for x in chain)
-        except nx.exception.NetworkXNoPath as ex:
-            pass
+        boundary = nx.edge_boundary(raw, left.nodes(), right.nodes())
+        if boundary:
+            yield left_name, right_name, boundary
+
 
 if __name__ == '__main__':
     if SQUELCH == 0:
@@ -30,9 +24,10 @@ if __name__ == '__main__':
 
     squelched = build_graph()
     raw = build_graph(0)
+    print ""
     for (left,right,chain) in sorted(find_bridge_edges(raw, squelched)):
         print left, "---->", right
-        for item in chain:
-            print "   ",item
+        for (begin,end) in chain:
+            print "   ",name_for(begin), name_for(end)
         print
 
