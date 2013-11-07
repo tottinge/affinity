ef6005609016d084863e3425a9458882ec3bb6f1
Tim Ottinger <tottinge@gmail.com>
2013-11-06 21:24:30 -0600

    path correlation prints now

--- display_path_correlation.py
+++ display_path_correlation.py
@@ -1,24 +1,35 @@
-
 import sys
 import os
-import sqlite3
 from difflib import SequenceMatcher
-from analyze_graph import *
+from analyze_graph import (
+    parse_command_line,
+    build_graph,
+    build_affinity_groups,
+    printable_edge
+)
+
 
 def path_commonality(p1, p2):
-    p1,_ = os.path.split(p1)
-    p2,_ = os.path.split(p2)
-    return "%d" % (SequenceMatcher(None, p1.split('/'), p2.split('/')).ratio() * 10)
+    p1, _ = os.path.split(p1)
+    p2, _ = os.path.split(p2)
+    commonality = SequenceMatcher(
+        None,
+        p1.split('/'),
+        p2.split('/')
+    ).ratio() * 10
+    return "%d" % (commonality,)
+
 
 def display_edges_and_path_commonality(subgraphs):
-    buckets = {}
     for (number, subgraph) in enumerate(subgraphs):
         for edge in subgraph.edges():
-            weight, left,right = printable_edge(subgraph, edge)
+            weight, left, right = printable_edge(subgraph, edge)
             print path_commonality(left, right), weight, left, right
         print ""
 
+
 if __name__ == '__main__':
-    G = build_graph()
-    subgraphs = build_affinity_groups(G)
+    args = parse_command_line(*sys.argv[1:])
+    G = build_graph(args.inputfile)
+    subgraphs = build_affinity_groups(G, args.squelch)
     display_edges_and_path_commonality(subgraphs)
--- runme.sh
+++ runme.sh
@@ -38,9 +38,9 @@ python combine_graphs.py ${OUT_DIR}/*.xml > ${COMBINED}
 #echo "Conducting analysis"
 python display_nodes.py ${SQUELCH} ${MIN_GROUP}  ${COMBINED} > ${OUT_DIR}/groups_by_file.txt
 python display_connectors.py ${SQUELCH} ${MIN_GROUP} ${COMBINED} > ${OUT_DIR}/connectors.txt
+python display_path_correlation.py ${SQUELCH} ${MIN_GROUP} ${COMBINED} > ${OUT_DIR}/path_correlation.txt
 
 #python display_edges.py ${SQUELCH} ${MIN_GROUP} ${COMBINED} > ${OUT_DIR}/groups_by_linkage.${DAYS}.txt
 #python display_spanning_edges.py ${SQUELCH} ${MIN_GROUP} ${COMBINED} > ${OUT_DIR}/spanning_edges.${DAYS}.txt
-#python display_path_correlation.py ${SQUELCH} ${MIN_GROUP} ${COMBINED} > ${OUT_DIR}/path_correlation.${DAYS}.txt
 
 echo "done"
