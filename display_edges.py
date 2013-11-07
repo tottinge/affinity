f000d3f8ab5fb182ce32fff3f73bda8df303f9ba
Tim Ottinger <tottinge@gmail.com>
2013-11-06 21:17:07 -0600

    display_edges is working

--- display_edges.py
+++ display_edges.py
@@ -1,17 +1,25 @@
 import sys
-import os
-import sqlite3
-from analyze_graph import *
+from analyze_graph import (
+    build_graph,
+    build_affinity_groups,
+    parse_command_line,
+    printable_edge
+)
+
 
 def display_result_edges(subgraphs):
-    for (number,subgraph) in enumerate(subgraphs):
+    for (number, subgraph) in enumerate(subgraphs):
         print "group %d of len %d" % (number, subgraph.number_of_edges())
-        sorted_by_weight = sorted( (printable_edge(subgraph,edge) for edge in subgraph.edges()), reverse=True)
-        for (weight,filename1, filename2) in sorted_by_weight:
+        sorted_by_weight = sorted(
+            (printable_edge(subgraph, edge) for edge in subgraph.edges()),
+            reverse=True
+        )
+        for (weight, filename1, filename2) in sorted_by_weight:
             print "\t", weight, filename1, filename2
         print ""
 
 if __name__ == '__main__':
-    G = build_graph()
-    subgraphs = build_affinity_groups(G)
+    args = parse_command_line(*sys.argv[1:])
+    G = build_graph(args.inputfile)
+    subgraphs = build_affinity_groups(G, args.squelch)
     display_result_edges(subgraphs)
